b1735e29b5e1f17fb28cdfb34e0e1357aea90ce7
Eshwar Nagaraj <eshwarnagaraj8099@gmail.com>
2023-04-25 09:48:04 +0530

    Practiced Destructuring of arrays using number array and string array

---
+++ destructuring-arrays.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const arr = [1,2,3];
+
+//Normal way of seperating array to variables
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+
+//Destructuring the array into separate variables
+//It is a ES6 feature
+//Upacks the array into separate variables
+const [x, y, z] = arr;
+console.log(x);
+console.log(y);
+console.log(z);
+
+const person = {
+    name: "Eshwar",
+    age: '23',
+    hobbies: ['Swimming', 'Dancing', 'Singing', 'Coding']
+}
+const [first, second] = person.hobbies;
+
+console.log(first);
+console.log(second);
